8be2140c1f866f801697c07e278c957604f5e32f
alisakkaf <support@alisakkaf.com>
2026-09-12 01:58:37 +0300

    Add Update v1.2.0 src/optimization_worker.cpp

--- src/optimization_worker.cpp
+++ src/optimization_worker.cpp
@@ -14,57 +14,7 @@ void OptimizationWorker::setTargetProcess(DWORD pid, const QString &procName) {
 }
 
 void OptimizationWorker::run() {
-    if (m_type == ActionMaximum) {
-        OptimizationResult res = m_cleaner.runMaximumOptimization(m_exclusions);
-        emit optimizationCompleted("Maximum Memory Optimization (Full System Flush)", res);
-    } else if (m_type == ActionQuick) {
-        OptimizationResult res = m_cleaner.runQuickOptimization(m_exclusions);
-        emit optimizationCompleted("Quick Trim (Process Working Sets Only)", res);
-    } else if (m_type == ActionPurgeStandby) {
-        MemoryMetrics before = m_cleaner.getMemoryMetrics();
-        m_cleaner.purgeStandbyList();
-
-        MemoryMetrics bestAfter = m_cleaner.getMemoryMetrics();
-        for (int i = 0; i < 5; ++i) {
-            QThread::msleep(400);
-            MemoryMetrics sample = m_cleaner.getMemoryMetrics();
-            if (sample.usedPhysBytes < bestAfter.usedPhysBytes) {
-                bestAfter = sample;
-            }
-        }
-
-        double purgedGB = (before.standbyCacheGB > bestAfter.standbyCacheGB) ? (before.standbyCacheGB - bestAfter.standbyCacheGB) : 0.0;
-        OptimizationResult res = {};
-        res.before = before;
-        res.after = bestAfter;
-        res.reclaimedGB = purgedGB;
-        res.reclaimedMB = purgedGB * 1024.0;
-        res.loadDiffPercent = before.memoryLoadPercent - bestAfter.memoryLoadPercent;
-
-        emit optimizationCompleted("Purge Standby Memory Cache Lists", res);
-    } else if (m_type == ActionFlushModified) {
-        MemoryMetrics before = m_cleaner.getMemoryMetrics();
-        m_cleaner.flushModifiedList();
-
-        MemoryMetrics bestAfter = m_cleaner.getMemoryMetrics();
-        for (int i = 0; i < 5; ++i) {
-            QThread::msleep(400);
-            MemoryMetrics sample = m_cleaner.getMemoryMetrics();
-            if (sample.usedPhysBytes < bestAfter.usedPhysBytes) {
-                bestAfter = sample;
-            }
-        }
-
-        double reclaimedGB = (before.usedPhysGB > bestAfter.usedPhysGB) ? (before.usedPhysGB - bestAfter.usedPhysGB) : 0.0;
-        OptimizationResult res = {};
-        res.before = before;
-        res.after = bestAfter;
-        res.reclaimedGB = reclaimedGB;
-        res.reclaimedMB = reclaimedGB * 1024.0;
-        res.loadDiffPercent = before.memoryLoadPercent - bestAfter.memoryLoadPercent;
-
-        emit optimizationCompleted("Flush Modified Memory Page Lists to Disk", res);
-    } else if (m_type == ActionTrimProcess) {
+    if (m_type == ActionTrimProcess) {
         MemoryMetrics before = m_cleaner.getMemoryMetrics();
         HANDLE hProc = OpenProcess(PROCESS_SET_QUOTA | PROCESS_QUERY_INFORMATION, FALSE, m_targetPid);
         bool ok = false;
@@ -86,5 +36,60 @@ void OptimizationWorker::run() {
         }
         MemoryMetrics after = m_cleaner.getMemoryMetrics();
         emit singleProcessTrimCompleted(m_targetPid, m_targetProcName, ok, savedMB, before.memoryLoadPercent, after.memoryLoadPercent);
+        return;
     }
+
+    MemoryMetrics before = m_cleaner.getMemoryMetrics();
+    int trimmed = 0, skipped = 0, prot = 0;
+    QString actionTitle;
+
+    if (m_type == ActionMaximum) {
+        actionTitle = "Maximum Memory Optimization (Full System Flush)";
+        m_cleaner.flushModifiedList();
+        m_cleaner.purgeStandbyList();
+        trimmed = m_cleaner.trimProcessWorkingSets(m_exclusions, &skipped, &prot);
+        m_cleaner.trimSystemWorkingSets();
+    } else if (m_type == ActionQuick) {
+        actionTitle = "Quick Trim (Process Working Sets Only)";
+        trimmed = m_cleaner.trimProcessWorkingSets(m_exclusions, &skipped, &prot);
+    } else if (m_type == ActionPurgeStandby) {
+        actionTitle = "Purge Standby Memory Cache Lists";
+        m_cleaner.purgeStandbyList();
+    } else if (m_type == ActionFlushModified) {
+        actionTitle = "Flush Modified Memory Page Lists to Disk";
+        m_cleaner.flushModifiedList();
+    }
+
+    emit settlingStarted(actionTitle);
+
+    MemoryMetrics bestAfter = m_cleaner.getMemoryMetrics();
+
+    // Exactly 10 seconds settling & sampling period (10 iterations x 1000ms)
+    for (int sec = 1; sec <= 10; ++sec) {
+        QThread::msleep(1000);
+        MemoryMetrics sample = m_cleaner.getMemoryMetrics();
+
+        if (sample.usedPhysBytes < bestAfter.usedPhysBytes) {
+            bestAfter = sample;
+        }
+
+        emit settlingTick(sec, 10);
+    }
+
+    OptimizationResult res = {};
+    res.before = before;
+    res.after = bestAfter;
+    if (before.usedPhysBytes > bestAfter.usedPhysBytes) {
+        res.reclaimedBytes = before.usedPhysBytes - bestAfter.usedPhysBytes;
+    } else {
+        res.reclaimedBytes = 0;
+    }
+    res.reclaimedMB = res.reclaimedBytes / (1024.0 * 1024.0);
+    res.reclaimedGB = res.reclaimedBytes / (1024.0 * 1024.0 * 1024.0);
+    res.loadDiffPercent = before.memoryLoadPercent - bestAfter.memoryLoadPercent;
+    res.trimmedProcesses = trimmed;
+    res.skippedProcesses = skipped;
+    res.protectedProcesses = prot;
+
+    emit optimizationCompleted(actionTitle, res);
 }
